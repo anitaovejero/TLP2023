e49c244695684e39096faf49cc3af5aef2488ab1
anitaovejero <126704963+anitaovejero@users.noreply.github.com>
2023-06-05 13:27:07 -0300

    Add files via upload

---
+++ candeyana59.py
@@ -0,0 +1,6 @@
+numeros= [9, 6, 12, 5, 66]
+
+for x in range (5):
+    if numeros[x]>=7:
+     print ("Los valores mayores o iguales a 7 son: ")
+     print (numeros[x])
---
+++ candeyana60.py
@@ -0,0 +1,6 @@
+nombres=["ana", "candelaria", "abril", "andres", "martin"]
+contador=0
+for nombres in nombres:
+    if len (nombres) > 5:
+        contador += 1
+print ("Cantidad de nombres con mas de 5 caracteres: ",contador)
