329dc0da42bb4f78d3e11be8f4ee80ec95cb0e03
anitaovejero <126704963+anitaovejero@users.noreply.github.com>
2023-06-26 13:18:14 -0300

    Add files via upload

---
+++ candeyana51.py
@@ -0,0 +1,8 @@
+cadena= "mAriA"
+print (cadena)
+cadena1=cadena.upper();
+print (cadena1)
+cadena2=cadena.lower();
+print (cadena2)
+cadena3=cadena.capitalize();
+print (cadena3)
---
+++ candeyana52.py
@@ -0,0 +1,13 @@
+espacio= " "
+contador=0
+contador1=0
+oracion=input ("Ingrese una oracion: ")
+if espacio in oracion:
+    contador1=contador+1
+    print ("Se ingresaron ·".contador2, " espacios.")
+cadena1=cadena.upper();
+print (cadena1)
+cadena2=cadena.lower();
+print (cadena2)
+cadena3=cadena.capitalize();
+print (cadena3)
